c3e6cfccb1b06edc67fe6f1d744fd129808296cf
brenman60 <83514481+brenman60@users.noreply.github.com>
2024-09-18 22:46:25 -0400

    Changes

--- Group Game Project 2024/Assets/Scripts/Interactables/Interacts/PizzaShopDoor.cs
+++ Group Game Project 2024/Assets/Scripts/Interactables/Interacts/PizzaShopDoor.cs
@@ -1,7 +1,16 @@
+using UnityEngine;
+
 public class PizzaShopDoor : Interactable
 {
+    [SerializeField] private bool exterior = true;
+    [SerializeField] private GameObject exteriorTeleportPoint;
+    [SerializeField] private GameObject interiorTeleportPoint;
+
     public override void Interact()
     {
-        
+        if (exterior)
+            Player.Instance.transform.position = interiorTeleportPoint.transform.position;
+        else
+            Player.Instance.transform.position = exteriorTeleportPoint.transform.position;
     }
 }
